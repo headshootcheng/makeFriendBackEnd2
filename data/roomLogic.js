11f6755d6477fce753a436e51ca74d14db562c71
Peter Cheng <petercheng7788@gmail.com>
2020-09-02 00:41:30 +0800

    no message

--- data/roomLogic.js
+++ data/roomLogic.js
@@ -9,9 +9,8 @@ const addUser = ({ id, username, userId, room_name }) => {
 };
 
 const removeUser = (id) => {
-  const index = users.findIndex((user) => user.id === id);
-
-  if (index !== -1) return users.splice(index, 1)[0];
+  const user = users.find((user) => user.id === id);
+  return user;
 };
 
 const getUser = (id) => users.find((user) => user.id === id);
